a84d00b80b79a41e4bb0ac2a1042620eb06eab4d
Oleksandr Sudak <a.sudak@avtok.com.ua>
2018-10-25 18:36:19 +0300

    init

---
+++ LiqpayBundle.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of the HH2P liqpay package.
+ *
+ * (c) Oleksanr Sudak <sswebdis@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace HH2P\Bundle\LiqpayBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+/**
+ * @author Oleksanr Sudak <sswebdis@gmail.com>
+ */
+class LiqpayBundle extends Bundle
+{
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+    }
+}
